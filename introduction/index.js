1002b335b24593dc1f1557bba75c5c7796e4c490
syahba <ksyaahba@gmail.com>
2023-11-17 06:54:46 +0700

    tugas introduction

---
+++ introduction/index.js
@@ -0,0 +1,25 @@
+// exercise js dasar - introduction
+
+// 1. console log data sendiri dan 3. single comments pada setiap baris
+console.log('syahba'); // nama
+console.log('depok'); // asal daerah
+console.log('01/04/2004'); // tanggal lahir
+console.log(19); // umur
+console.log('mahasiswa'); // posisi bekerja
+
+// 5. variable menampung data diri
+const myData = { 
+    name: 'syahba',
+    city: 'depok',
+    date: '01/04/2004',
+    age: 19,
+    position: 'mahasiswa'
+};
+console.log(myData);
+
+// 6. operasi matematika dan masing-masing operasi disimpan di variable sendiri
+const addition = 25 + 10;
+const multiplication = 100 *2;
+const division = 99 / 2;
+const modular = 99 % 2;
+console.log(addition, multiplication, division, modular);
