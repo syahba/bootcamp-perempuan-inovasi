b806e10da2378ee5229b406c0ead724353a95913
syahba <ksyaahba@gmail.com>
2023-11-29 10:45:34 +0700

    web storage task

---
+++ web-storage/script.js
@@ -0,0 +1,33 @@
+const data = localStorage.getItem('todos'); // get data from local storage
+
+// show existing data on load
+window.addEventListener('DOMContentLoaded', () => {
+    const list = document.getElementById('todo-list');
+    const res = JSON.parse(data);
+    res.map(value => {
+        list.innerHTML += `<li>${value}</li>`;
+    })
+})
+
+// submit button action
+const submit = document.getElementById('submit');
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    saveTodo();
+});
+
+// save data to local storage from input
+let arr = [];
+const saveTodo = () => {
+    data ? arr = JSON.parse(data) : arr = [];
+    const todo = document.getElementById('todo').value;
+    arr.push(todo);
+    localStorage.setItem('todos', JSON.stringify(arr));
+    addTodo(todo);
+}
+
+// add new input data to existing list
+const addTodo = (todo) => {
+    const list = document.getElementById('todo-list');
+    list.innerHTML += `<li>${todo}</li>`;
+}
